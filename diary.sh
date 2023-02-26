ca4716cb975951db9a3b4d1a8843591e94b71563
Diego Milan <diego.088@gmail.com>
2023-02-26 11:34:30 -0600

    Fix

--- diary.sh
+++ diary.sh
@@ -10,13 +10,14 @@ fi
 datestamp=$(date +"%Y-%m-%d")
 timestamp=$(date +"%I:%M %p")
 
+
 # Check if there are any entries in the diary file for the current date
-if grep -q "^$datestamp$" ~/diary.txt; then
+if grep -q "^$datestamp$" "${DIARY_DIR}/diary.txt"; then
   # If there are already entries for the current date, just append the new entry with a timestamp
-  echo -e "\n$timestamp\n$*" >> ~/diary.txt
+  echo -e "\n$timestamp\n$*" >> "${DIARY_DIR}/diary.txt"
 else
   # If it's a new date, append the date and timestamp along with the new entry
-  echo -e "\n\n$datestamp\n\n$timestamp\n$*" >> ~/diary.txt
+  echo -e "\n\n$datestamp\n\n$timestamp\n$*" >> "${DIARY_DIR}/diary.txt"
 fi
 
 # Print a confirmation message
